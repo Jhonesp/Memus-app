ebafc3c76c2082314a788d73b3e1f80341b3dec4
Jhon Esparragoza <jhonantonyesp@gmail.com>
2024-05-14 16:49:17 +0200

    Fondo refactorizado a componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import styles from './App.module.css'
 import Header from './components/Header'
 import NotasContainer from './components/NotasContainer'
+import Fondo from './components/Fondo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,21 +12,9 @@ function App() {
     <div className={styles.container}>
         <Header />
         <NotasContainer />
+        <Fondo />
     </div>
-    <div className={styles.area} >
-            <ul className={styles.circles}>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
+    
     </>
   )
 }
---
+++ src/components/Fondo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Fondo.module.css'
+
+const Fondo = () => {
+  return (
+    <div className={styles.area}>
+        <ul className={styles.circles}>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div>
+  )
+}
+
+export default Fondo
